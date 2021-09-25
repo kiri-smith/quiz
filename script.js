0a9121a0885dfba7f124ac8f7146c7c0506ec888
Kiri <kirilynne@gmail.com>
2021-09-25 13:41:03 -0400

    added to script.js to control visibility for questions

--- script.js
+++ script.js
@@ -1,9 +1,101 @@
 var startButton = document.querySelector(".start-button");
 
+var questions = [
+    {
+        question: "What liquor is used in a traditional margarita?",
+        a: "vodka",
+        b: "rum",
+        c: "tequila",
+        d: "whiskey",
+        correctAnswer: "a"
+    },
+    {
+        question: "question 2?",
+        a: "vodka",
+        b: "rum",
+        c: "tequila",
+        d: "whiskey",
+        correctAnswer: "a"
+    },
+    {
+        question: "question 3?",
+        a: "vodka",
+        b: "rum",
+        c: "tequila",
+        d: "whiskey",
+        correctAnswer: "a"
+    },
+    {
+        question: "question 4?",
+        a: "vodka",
+        b: "rum",
+        c: "tequila",
+        d: "whiskey",
+        correctAnswer: "a"
+    },
+    {
+        question: "What liquor is used in a traditional margarita?",
+        a: "vodka",
+        b: "rum",
+        c: "tequila",
+        d: "whiskey",
+        correctAnswer: "a"
+    },
+    {
+        question: "What liquor is used in a traditional margarita?",
+        a: "vodka",
+        b: "rum",
+        c: "tequila",
+        d: "whiskey",
+        correctAnswer: "a"
+    },
+    {
+        question: "What liquor is used in a traditional margarita?",
+        a: "vodka",
+        b: "rum",
+        c: "tequila",
+        d: "whiskey",
+        correctAnswer: "a"
+    },
+    {
+        question: "What liquor is used in a traditional margarita?",
+        a: "vodka",
+        b: "rum",
+        c: "tequila",
+        d: "whiskey",
+        correctAnswer: "a"
+    },
+    {
+        question: "What liquor is used in a traditional margarita?",
+        a: "vodka",
+        b: "rum",
+        c: "tequila",
+        d: "whiskey",
+        correctAnswer: "a"
+    },
+    {
+        question: "question 10?",
+        a: "vodka",
+        b: "rum",
+        c: "tequila",
+        d: "whiskey",
+        correctAnswer: "a"
+    },
+]
 
 //push start button
 
 startButton.addEventListener("click", startQuiz);
+var questionContainer = document.querySelector(".question-container");
+var highScoresContainer = document.querySelector(".high-scores-container");
+var countdownContainer = document.querySelector(".countdown-timer-container");
+var startContainer = document.querySelector(".start-container")
+
+function startQuiz(){
+    startContainer.classList.add("hidden");
+    questionContainer.classList.remove("hidden");
+    countdownContainer.classList.remove("hidden");
+}
 
 //then timer begins countdown and question-one appears
 
