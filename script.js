7a57c58026ef942bef04ac7baeff9858cfd0b528
Kiri <kirilynne@gmail.com>
2021-09-25 14:38:58 -0400

    implemented end quiz login

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var questions = [
         b: "rum",
         c: "tequila",
         d: "whiskey",
-        correctAnswer: "a"
+        correctAnswer: "c"
     },
     {
         question: "question 2?",
@@ -95,13 +95,48 @@ var questionAnswerAEl = document.querySelector(".question-answer-a");
 var questionAnswerBEl = document.querySelector(".question-answer-b");
 var questionAnswerCEl = document.querySelector(".question-answer-c");
 var questionAnswerDEl = document.querySelector(".question-answer-d");
+var countdownTimerEl =  document.querySelector(".countdown-timer");
 var currentQuestionIndex = 0;
+var timer = 0;
+var timerTicker;
+var highScores = JSON.parse(localStorage.getItem("high-scores")) || [];
 
 function startQuiz(){
     startContainer.classList.add("hidden");
     questionContainer.classList.remove("hidden");
     countdownContainer.classList.remove("hidden");
     displayQuestion();
+    timer = 180;
+    countdownTimerEl.textContent = timer;
+    timerTicker = setInterval(tickTimer, 1000);
+}
+
+function endQuiz(){
+    questionContainer.classList.add("hidden");
+    countdownContainer.classList.add("hidden");
+    highScoresContainer.classList.remove("hidden");
+    clearInterval(timerTicker);
+    var initials = prompt("Enter your initials");
+    highScores.push({
+        initials: initials,
+        score: timer,
+    })
+    highScores.sort(compareHighScores);
+    localStorage.setItem("high-scores", JSON.stringify(highScores));
+    displayHighScores();
+}
+
+function compareHighScores(a, b){
+    return b.score - a.score;
+}
+
+function tickTimer(){
+    if (timer <= 0){
+        countdownTimerEl.textContent = timer;
+        endQuiz();
+    }
+    timer--;
+    countdownTimerEl.textContent = timer;
 }
 
 function displayQuestion(){
@@ -113,13 +148,33 @@ function displayQuestion(){
     questionAnswerDEl.textContent = "d. " + currentQuestion.d;
 }
 
+function displayHighScores(){
+    while (highScoresContainer.firstChild){
+        highScoresContainer.removeChild(highScoresContainer.firstChild);
+    }
+    for (var highScore of highScores) {
+        const container=document.createElement("div");
+        const nameEl=document.createElement("h2");
+        const scoreEl=document.createElement("h2");
+        nameEl.textContent=highScore.initials;
+        scoreEl.textContent=highScore.score;
+        container.appendChild(nameEl);
+        container.appendChild(scoreEl);
+        highScoresContainer.appendChild(container);
+    }
+}
+
 function submitAnswer(answer){
     var correctAnswer = questions[currentQuestionIndex].correctAnswer;
     if (answer!==correctAnswer){
-        //TODO: Subtract time from clock
+        timer-=10; 
     }
     currentQuestionIndex++;
-    displayQuestion();
+    if (currentQuestionIndex >= questions.length){
+        endQuiz();
+    } else {
+        displayQuestion();
+    }
 }
 //then timer begins countdown and question-one appears
 
