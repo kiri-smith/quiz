6c60838b0b268e953d280357339f0d20adbd683b
Kiri <kirilynne@gmail.com>
2021-09-25 13:52:33 -0400

    created display question logic

--- script.js
+++ script.js
@@ -89,12 +89,28 @@ startButton.addEventListener("click", startQuiz);
 var questionContainer = document.querySelector(".question-container");
 var highScoresContainer = document.querySelector(".high-scores-container");
 var countdownContainer = document.querySelector(".countdown-timer-container");
-var startContainer = document.querySelector(".start-container")
+var startContainer = document.querySelector(".start-container");
+var questionTextEl = document.querySelector(".question-text");
+var questionAnswerAEl = document.querySelector(".question-answer-a");
+var questionAnswerBEl = document.querySelector(".question-answer-b");
+var questionAnswerCEl = document.querySelector(".question-answer-c");
+var questionAnswerDEl = document.querySelector(".question-answer-d");
+var currentQuestionIndex = 0;
 
 function startQuiz(){
     startContainer.classList.add("hidden");
     questionContainer.classList.remove("hidden");
     countdownContainer.classList.remove("hidden");
+    displayQuestion();
+}
+
+function displayQuestion(){
+    var currentQuestion = questions[currentQuestionIndex];
+    questionTextEl.textContent = currentQuestion.question;
+    questionAnswerAEl.textContent = "a. " + currentQuestion.a;
+    questionAnswerBEl.textContent = "b. " + currentQuestion.b;
+    questionAnswerCEl.textContent = "c. " + currentQuestion.c;
+    questionAnswerDEl.textContent = "d. " + currentQuestion.d;
 }
 
 //then timer begins countdown and question-one appears
